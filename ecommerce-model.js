4afd6025f29d2120740c3332fb5cff35fd4e6c40
Rashed Khan Menon <128370660+rkm-dev7@users.noreply.github.com>
2023-09-02 17:39:30 +0600

    Add files via upload
    Building E-Commerce Models with Mongoose

---
+++ ecommerce-model.js
@@ -0,0 +1,123 @@
+const mongoose = require("mongoose");
+
+// User Model
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  address: String,
+  phoneNumber: String,
+});
+
+// Product Model
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  stock: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  imageURL: String,
+});
+
+// CartItem Model
+const cartItemSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1,
+  },
+});
+
+// Order Model
+const orderSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  items: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+        min: 1,
+      },
+    },
+  ],
+  totalAmount: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  shippingAddress: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+    default: "Pending",
+  },
+});
+
+// middleware to remove cart items and orders when a user is deleted
+userSchema.pre("remove", async function (next) {
+  await CartItem.deleteMany({ user: this._id });
+  await Order.deleteMany({ user: this._id });
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+const Product = mongoose.model("Product", productSchema);
+const CartItem = mongoose.model("CartItem", cartItemSchema);
+const Order = mongoose.model("Order", orderSchema);
+
+module.exports = {
+  User,
+  Product,
+  CartItem,
+  Order,
+};
